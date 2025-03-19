ddb3730ea785dc6bb517b8f3748207323b67bab7
caopeng <caopeng_404@163.com>
2025-03-19 17:45:48 +0800

    完成0319

---
+++ src/shader/0319/frag.glsl
@@ -0,0 +1,47 @@
+precision highp float;
+varying vec2 vUv;
+uniform vec2 uResolution;
+uniform float uTime;
+#define NUM_OCTAVES 5
+
+float rand(vec2 n) {
+    return fract(sin(dot(n, vec2(12.9898,4.1414))) *43758.5453); 
+}
+float noise(vec2 p){ 
+    vec2 ip = floor(p);
+    vec2 u = fract(p);
+    u = u*u*(3.0-2.0*u);
+    float res = mix( mix(rand(ip), rand(ip+vec2(1.0,0.0)), u.x), mix(rand(ip+vec2(0.0,1.0)), rand(ip+vec2(1.0,1.0)), u.x), u.y);
+    return res*res;
+ }
+ float fbm(vec2 x){
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100);
+    mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+    for(int i =0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(x);
+        x = rot * x *2.0+ shift;
+         a *=0.5;
+    }
+    return v;
+}
+
+void main () {
+    vec2 shake = vec2(sin(uTime *1.5) *0.01, cos(uTime *2.7) *0.01);
+    vec2 fragCoord = vUv * uResolution;
+    vec2 p = ((fragCoord + shake * uResolution) - uResolution *0.5) / uResolution.y;
+    p *= mat2(8.0,-6.0,6.0,8.0);
+    vec2 v;
+    vec4 o = vec4(0.0);
+    float f =3.0+ fbm(p + vec2(uTime *7.0,0.0));
+    for(float i =0.0; i <50.0; i++) {
+        v = p + cos(i*i + (uTime + p.x*0.1)*0.03+ i*vec2(11.0,9.0)) *5.0 + vec2(sin(uTime*4.0+ i)*0.005, cos(uTime*4.5- i)*0.005);
+        float tailNoise = fbm(v + vec2(uTime, i)) * (1.0- (i/50.0));
+        vec4 currentContribution = (cos(sin(i)*vec4(1.0,2.0,3.0,1.0)) +1.0) * exp(sin(i*i + uTime)) / length(max(v, vec2(v.x*f*0.02, v.y)));
+        float thinnessFactor = smoothstep(0.0,1.0, i/50.0);
+        o += currentContribution * (1.0 + tailNoise*2.0) * thinnessFactor;
+    }
+    o = tanh(pow(o/1e2, vec4(1.5)));
+    gl_FragColor = o;
+}
--- src/view/0319/index.vue
+++ src/view/0319/index.vue
@@ -1,7 +1,72 @@
+<!--
+ * @Author: caopeng
+ * @Date: 2025-03-19 14:58:09
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2025-03-19 17:42:51
+ * @Description: 请填写简介
+-->
 <template>
-  <div>你好</div>
+  <div class="canvasRef" ref="canvasRef"></div>
 </template>
 
-<script setup></script>
+<script setup>
+import * as THREE from "three";
+import { onMounted, ref } from "vue";
+// 引入控制轴
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-<style lang="scss" scoped></style>
+// 引入片元着色器
+import fragmentShader from "../../shader/0319/frag.glsl?raw";
+// 顶点着色器
+import vertexShader from "../../shader/0319/vert.glsl?raw";
+const canvasRef = ref(null);
+let scene, camera, renderer, controls;
+onMounted(() => {
+  initScene();
+  animate();
+});
+const uni = {
+  uTime: { value: 0 },
+  uResolution: { value: new THREE.Vector2() },
+};
+const initScene = () => {
+  scene = new THREE.Scene();
+  camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  canvasRef.value.appendChild(renderer.domElement);
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // 创建物体
+  const mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, 2),
+    new THREE.ShaderMaterial({
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+      fragmentShader,
+      vertexShader,
+      uniforms: uni,
+    })
+  );
+  scene.add(mesh);
+};
+
+const animate = () => {
+  uni.uTime.value = performance.now() / 1000;
+  uni.uResolution.value.set(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  controls.update();
+  requestAnimationFrame(animate);
+};
+</script>
+
+<style lang="scss" scoped>
+.canvasRef {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
