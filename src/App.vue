771f99441551cb5de4f6716b605bde439e03ed74
caopeng <caopeng_404@163.com>
2025-03-19 15:10:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-content">
+    <scene />
+  </div>
+</template>
+
+<script setup>
+import scene from "./view/0319/index.vue";
+</script>
+
+<style lang="scss" scoped>
+.app-content {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  top: 0;
+  left: 0;
+}
+</style>
