0992ec711fe613e0b54a3c6ee34daf7314579b18
caopeng <caopeng_404@163.com>
2025-03-23 16:00:03 +0800

    完成修改

--- src/shader/0322/rayVert.glsl
+++ src/shader/0322/rayVert.glsl
@@ -1,6 +1,6 @@
- attribute float aSize;
- attribute vec3 aVelocity;
- attribute float aLifeProgress;
+ attribute float size;
+ attribute vec3 velocity;
+ attribute float lifeProgress;
  varying vec3 vColor;
  varying float vLife;
  uniform float uTime;
@@ -8,11 +8,11 @@
 
  void main(){
         vColor = color;
-        float progress = mod(aLifeProgress + uTime * 0.5, 1.0);
+        float progress = mod(lifeProgress + uTime * 0.5, 1.0);
         vLife = 1.0 - progress;
-        vec3 animatedPosition = position + aVelocity * progress * 4.0;
-        float flicker = sin(uTime * 15.0 + aLifeProgress * 25.0) * 0.3 + 1.0;
+        vec3 animatedPosition = position + velocity * progress * 4.0;
+        float flicker = sin(uTime * 15.0 + lifeProgress * 25.0) * 0.3 + 1.0;
         vec4 mvPosition = modelViewMatrix * vec4(animatedPosition, 1.0);
-        gl_PointSize = aSize * vLife * flicker * (500.0 / -mvPosition.z);
+        gl_PointSize = size * vLife * flicker * (500.0 / -mvPosition.z);
         gl_Position = projectionMatrix * mvPosition;
  }
