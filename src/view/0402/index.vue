7863569c302e2ca924ba8b5fc02eb54f42c96aaa
caopeng <caopeng_404@163.com>
2025-04-02 09:25:10 +0800

    初始化物体

--- src/view/0402/index.vue
+++ src/view/0402/index.vue
@@ -2,7 +2,7 @@
  * @Author: caopeng
  * @Date: 2025-04-02 09:01:16
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2025-04-02 09:06:30
+ * @LastEditTime: 2025-04-02 09:24:06
  * @Description: 请填写简介
 -->
 <template>
@@ -16,6 +16,16 @@ import { ref, onMounted } from "vue";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 const canvasRef = ref(null);
 let scene, camera, renderer, controls; // 场景、相机、渲染器、控制器
+let colors = ["#da6b00", "#8555d4", "#4ad3b5", "#ffffff"];
+let radius = 5; // 圆的半径
+let mouse = new THREE.Vector2(); // 鼠标位置
+let canvasMouse = new THREE.Vector3(0, 0, 0); // 画布上的鼠标位置
+let spheresInfo = {}; // 球的信息
+let sphereIndex = 0; // 球的索引
+let parent = new THREE.Object3D(); // 父对象
+let floorDimensions = 350; // 地板尺寸
+let floorLevel = -100; // 地板位置
+let floor; // 地板
 onMounted(() => {
   init();
   animate();
@@ -25,19 +35,42 @@ const init = () => {
   scene = new THREE.Scene();
   // 创建相机
   camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
   // 设置相机位置
-  camera.position.set(0, 0, 5);
+  camera.position.z = 600;
   // 创建渲染器
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setClearColor("#4e4d70");
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
   canvasRef.value.appendChild(renderer.domElement);
+  // 添加灯光
+  const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+  scene.add(ambientLight);
   // 创建轨道控制器
   controls = new OrbitControls(camera, renderer.domElement);
+  // 监听性能
+
+  // 添加物体
+  const geometry = new THREE.BufferGeometry(
+    floorDimensions,
+    5,
+    floorDimensions
+  );
+  const material = new THREE.MeshLambertMaterial({
+    color: "#000000",
+    depthWrite: false,
+    transparent: true, // 开启透明
+    opacity: 0.2, // 设置透明度
+  });
+  floor = new THREE.Mesh(geometry, material);
+  floor.position.y = floorLevel - radius / 2;
+  floor.position.z = 0;
+  scene.add(floor);
 };
 const animate = () => {
   requestAnimationFrame(animate);
