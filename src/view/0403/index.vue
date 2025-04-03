17da841109419639f2fc5b72f484c2a2da12075c
caopeng <caopeng_404@163.com>
2025-04-03 09:30:31 +0800

    创建物体和灯光

--- src/view/0403/index.vue
+++ src/view/0403/index.vue
@@ -11,6 +11,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // Vue ref引用，用于挂载Three.js渲染器DOM元素
 const canvasRef = ref(null);
 let renderer, scene, camera, controls;
+let circle, particle, halo, luminor;
 onMounted(() => {
   init();
   animate();
@@ -27,18 +28,94 @@ const init = () => {
     1000 // 远裁剪面
   );
   // 设置相机位置
-  camera.position.set(0, 0, 10);
+  camera.position.z = 400;
   // 创建渲染器
   renderer = new THREE.WebGLRenderer({
     antialias: true, // 抗锯齿
     alpha: true, // 透明背景
   });
+  renderer.setPixelRatio(window.devicePixelRatio);
   // 设置渲染器大小
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.autoClear = false;
+  renderer.setClearColor(0x000000, 0.0);
   // 将渲染器DOM元素添加到Vue ref引用的DOM元素中
   canvasRef.value.appendChild(renderer.domElement);
   // 创建轨道控制器
   controls = new OrbitControls(camera, renderer.domElement);
+  // 添加物体
+  circle = new THREE.Object3D();
+  particle = new THREE.Object3D();
+  halo = new THREE.Object3D();
+  luminor = new THREE.Object3D();
+
+  scene.add(circle);
+  scene.add(particle);
+  scene.add(halo);
+  scene.add(luminor);
+
+  var geometry = new THREE.TetrahedronGeometry(1, 1);
+  var geo_planet = new THREE.SphereGeometry(10, 64, 32);
+  var geom3 = new THREE.SphereGeometry(16, 32, 16);
+  var geo_star = new THREE.SphereGeometry(90, 64, 64);
+
+  var material = new THREE.MeshPhongMaterial({
+    color: 0x111111,
+    shading: THREE.FlatShading,
+  });
+
+  for (var i = 0; i < 500; i++) {
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position
+      .set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
+      .normalize();
+    mesh.position.multiplyScalar(200 + Math.random() * 500);
+    mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+    particle.add(mesh);
+  }
+  var mat = new THREE.MeshPhongMaterial({
+    color: 0xe3d1af,
+    emissive: 0x000000,
+    shading: THREE.SmoothShading,
+    map: THREE.ImageUtils.loadTexture(
+      "https://upload.wikimedia.org/wikipedia/commons/2/2c/Generic_Celestia_asteroid_texture.jpg"
+    ),
+    bumpMap: THREE.ImageUtils.loadTexture(
+      "https://upload.wikimedia.org/wikipedia/commons/2/2c/Generic_Celestia_asteroid_texture.jpg"
+    ),
+    bumpScale: 0.025,
+    specularMap: THREE.ImageUtils.loadTexture(
+      "https://upload.wikimedia.org/wikipedia/commons/2/2c/Generic_Celestia_asteroid_texture.jpg"
+    ),
+    specular: new THREE.Color("grey"),
+  });
+  var mat3 = new THREE.ShaderMaterial({
+    uniforms: {},
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+    side: THREE.BackSide,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+  });
+  var planet = new THREE.Mesh(geo_planet, mat);
+  planet.scale.x = planet.scale.y = planet.scale.z = 15;
+  circle.add(planet);
+
+  var ball = new THREE.Mesh(geom3, mat3);
+  ball.scale.x = ball.scale.y = ball.scale.z = 16;
+  halo.add(ball);
+
+  var ball2 = new THREE.Mesh(geom3, mat3);
+  ball2.scale.x = ball2.scale.y = ball2.scale.z = 12;
+  ball2.position.set(25, 5, 1);
+  halo.add(ball2);
+
+  var ambientLight = new THREE.AmbientLight(0x000000);
+  scene.add(ambientLight);
+
+  var hemiLight = new THREE.HemisphereLight(0x000000, 0x1111111, 20);
+  hemiLight.position.set(-1, -1, 2);
+  luminor.add(hemiLight);
 };
 const animate = () => {
   // 渲染场景和相机
