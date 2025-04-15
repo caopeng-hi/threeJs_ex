a088be0ba573a64cd79a52b6ea3eef870775122b
caopeng <caopeng_404@163.com>
2025-04-15 13:06:49 +0800

    完善场景渲染器

--- src/view/0415/index.vue
+++ src/view/0415/index.vue
@@ -16,23 +16,37 @@ onMounted(() => {
 const init = () => {
   // 创建场景
   scene = new THREE.Scene();
-  const bgColor = new THREE.Color(0.5, 0.5, 0.5); // 设置背景颜色
-  scene.background = bgColor;
+  scene.background = new Color(0x000000);
+  scene.background.setRGB(0.01, 0.005, 0.05);
+  scene.backgroundBlurriness = 0.5;
   // 创建相机
   camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+    1,
+    2000
   );
-  camera.position.set(0, 0, 5);
+  camera.position.set(-2.95, 1.33, 3.91);
   // 创建渲染器
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer = new THREE.WebGLRenderer({
+    canvas: container,
+    antialias: true, //抗锯齿
+  });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.outputColorSpace = THREE.SRGBColorSpace;
   canvasRef.value.appendChild(renderer.domElement);
   // 创建控制器
   controls = new OrbitControls(camera, renderer.domElement);
+
+  // 创建物体
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  const material = new THREE.MeshBasicMaterial({
+    color: "yellow",
+  });
+  const cube = new Mesh(geometry, material);
 };
 const animate = () => {
   requestAnimationFrame(animate);
